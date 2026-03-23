08674ddfdbbdf5e89405185be93967ef652f113a
YoonDaeSung-01 <daesung_yoon59@naver.com>
2026-03-23 23:54:13 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.02 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/README.md
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 잘라서 배열로 저장하기 - 120913 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120913?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120913) 
 
 ### 성능 요약
 
-메모리: 9.22 MB, 시간: 0.03 ms
+메모리: 9.25 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 23일 23:53:22
+2026년 03월 23일 23:54:11
 
 ### 문제 설명
 
--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
@@ -1,9 +1,5 @@
 def solution(my_str, n):
     answer = []
-    for i in range(0, len(my_str)//n+1):
-        if (i+1)*n <= len(my_str):
-            answer.append(my_str[i*n : (i+1)*n])
-        else:
-            if len(my_str) % i*n <= n and len(my_str) % (i * n) !=0:
-                answer.append(my_str[i*n: len(my_str)])
+    for i in range(0, len(my_str), n):
+        answer.append(my_str[i:i+n])
     return answer
