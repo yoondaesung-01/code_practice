301cb31c0e15f058fdd05538eaf107f71e6ebd82
YoonDaeSung-01 <daesung_yoon59@naver.com>
2026-03-23 15:18:37 +0900

    [level 0] Title: 합성수 찾기, Time: 0.48 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -1,12 +1,11 @@
 def solution(n):
     answer = []
-    
     for i in range(4, n+1):
         count = 0
-        for j in range(1, i+1):
+        for j in range(1, int(i**0.5) +1):
             if i%j == 0:
                 count+=1
-                if count >= 3:
+                if count >= 2:
                     answer.append(i)
                     break
     return len(answer)
