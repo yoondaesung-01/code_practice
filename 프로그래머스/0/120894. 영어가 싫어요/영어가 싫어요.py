1249c0e484001cda433baa23d9d5f28791860f0d
YoonDaeSung-01 <daesung_yoon59@naver.com>
2026-03-24 10:41:55 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.02 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    num = {
+        
+        "zero": "0", "one" : "1", "two": "2", "three": "3", "four" : "4",
+        "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"
+    }
+    for key, value in num.items():
+        numbers = numbers.replace(key, value)
+    return int(numbers)
