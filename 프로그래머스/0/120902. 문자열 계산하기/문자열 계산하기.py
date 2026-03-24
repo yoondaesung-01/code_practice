59390a85ce32a070a704abbf5ff4bbf022017881
YoonDaeSung-01 <daesung_yoon59@naver.com>
2026-03-24 09:28:11 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.03 ms, Memory: 9.45 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,2 +1,13 @@
 def solution(my_string):
-    return eval(my_string)
+    my_list = my_string.split(" ")
+    cal = []
+    answer= int(my_list[0])
+    for i in range(1, len(my_list)):
+        cal.append(my_list[i])
+        if len(cal) == 2:
+            if cal[0] == "+":
+                answer += int(cal[1])
+            else:
+                answer -= int(cal[1])
+            cal = []
+    return answer
