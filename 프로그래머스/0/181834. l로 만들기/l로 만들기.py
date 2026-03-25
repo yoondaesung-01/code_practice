f9591f4236e6599ce0d665ba88c281d93321349d
YoonDaeSung-01 <daesung_yoon59@naver.com>
2026-03-25 15:19:21 +0900

    [level 0] Title: l로 만들기, Time: 5.18 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.py
@@ -1,9 +1,3 @@
 def solution(myString):
-    alphabet = ['a','b','c','d','e','f','g','h','i','j','k']
-    myList = []
-    for i in range(len(myString)):
-        if myString[i] in alphabet:
-            myList.append('l')
-        else:
-            myList.append(myString[i])
-    return ''.join(myList)
+    answer = [x if x > 'l' else 'l' for x in myString]
+    return ''.join(answer)
