8c23b5939d8e35e4074c98f08fb8d4c8ecb844a0
YoonDaeSung-01 <daesung_yoon59@naver.com>
2026-04-13 11:35:09 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.51 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,14 +1,4 @@
+from collections import Counter
 def solution(participant, completion):
-    answer = ''
-    part_dict={}
-    for i in participant:
-        if i in part_dict:
-            part_dict[i] += 1
-        else:
-            part_dict[i] = 1
-    for i in completion:
-        part_dict[i] -= 1
-    
-    for i in part_dict:
-        if part_dict[i] !=0: 
-            return i
+    result = Counter(participant) - Counter(completion)
+    return list(result.keys())[0]
